767d10215600dbaf002bddb040610694028ea3e5
Pawel Godula <bolomaster7@gmail.com>
2023-03-19 20:08:31 +0100

    employees routes and model changed

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -64,7 +64,7 @@ router.put('/employees/:id', async (req, res) => {
         }
       );
       res.json({ message: 'OK' });
-    } else res.status(404).json({ message: 'Not found...' });
+    } else res.status(404).json({ message: "Not found..." });
   } catch (err) {
     res.status(500).json({ message: err });
   }
