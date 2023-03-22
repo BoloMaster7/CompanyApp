9c5b71238e6bc7b6a3b3027a41a908061aa8b78a
Pawel Godula <bolomaster7@gmail.com>
2023-03-22 19:25:08 +0100

    create and update date tests add

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -14,6 +14,8 @@ describe('Department', () => {
     }
   });
 
+
+  ////READ
   describe('Reading data', () => {
 
     before(async () => {
@@ -39,6 +41,59 @@ describe('Department', () => {
     after(async () => {
       await Department.deleteMany();
     });
+  });
+
+  //////CREATE
+  describe('Creating data', () => {
+
+    it('should insert new document with "insertOne" method', async () => {
+      const department = new Department({ name: 'Department #1' });
+      await department.save();
+      expect(department.isNew).to.be.false;
+    });
+
+    after(async () => {
+      await Department.deleteMany();
+    })
+  
+  });
+
+  /////UPDATE
+  describe('Updating data', () => {
+
+    beforeEach(async () => {
+      const testDepOne = new Department({ name: 'Department #1' });
+      await testDepOne.save();
+    
+      const testDepTwo = new Department({ name: 'Department #2' });
+      await testDepTwo.save();
+    });
+
+    it('should properly update one document with "updateOne" method', async () => {
+      await Department.updateOne({ name: 'Department #1' }, { $set: { name: '=Department #1=' }});
+      const updatedDepartment = await Department.findOne({ name: '=Department #1=' });
+      expect(updatedDepartment).to.not.be.null;
+    });
+  
+    it('should properly update one document with "save" method', async () => {
+      const department = await Department.findOne({ name: 'Department #1' });
+      department.name = '=Department #1=';
+      await department.save();
+    
+      const updatedDepartment = await Department.findOne({ name: '=Department #1=' });
+      expect(updatedDepartment).to.not.be.null;
+    });
+  
+    it('should properly update multiple documents with "updateMany" method', async () => {
+      await Department.updateMany({}, { $set: { name: 'Updated!' }});
+      const departments = await Department.find({ name: 'Updated!' });
+      expect(departments.length).to.be.equal(2);
+    });
+
+    afterEach(async () => {
+      await Department.deleteMany();
+    });
+  
   });
 
 
